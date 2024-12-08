35bcb4ef61b07ffea45b7c560dfe9adacb1796b0
poulakise <poulakise@interamerican.gr>
2024-12-08 05:46:45 +0200

    feat(project): initial project structure.

---
+++ src/test/java/gr/evansp/spring/security/template/SpringSecurityTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.evansp.spring.security.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
